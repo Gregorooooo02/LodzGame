d93773d8696650d13d36c78ef0c30f8c65b19e67
Mateusz Podgórski <bazyl0022@gmail.com>
2025-11-23 17:26:03 +0100

    Switching to own voronoi implementation

--- Source/LevelGeneration/LevelGeneration.Build.cs
+++ Source/LevelGeneration/LevelGeneration.Build.cs
@@ -8,7 +8,7 @@ public class LevelGeneration : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","Voronoi" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
