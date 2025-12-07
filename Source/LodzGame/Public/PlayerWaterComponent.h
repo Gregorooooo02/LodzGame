71160ace210a21a18c5c1d9825d4e06a3f9f0966
Grzegorz Bednarek <gregorooooo02@gmail.com>
2025-12-07 15:16:52 +0100

    feat: Sprinting now works with PlayerWaterComponent; will work on swimming now

--- Source/LodzGame/Public/PlayerWaterComponent.h
+++ Source/LodzGame/Public/PlayerWaterComponent.h
@@ -6,26 +6,28 @@
 #include "Components/ActorComponent.h"
 #include "PlayerWaterComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnSwimmingStateChanged);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class LODZGAME_API UPlayerWaterComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UPlayerWaterComponent();
-
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
+	float WalkSpeed = 230.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
+	float SprintSpeed = 600.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
 	float MinSpeedMultiplier = 0.5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
+	float MinSprintSpeedMultiplier = 0.6f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
 	float MinJumpMultiplier = 0.5f;
@@ -34,7 +36,7 @@ public:
 	float MinGravityMultiplier = 0.5f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
-	class AWaterLevelManager* WaterLevelManager = nullptr;
+	class AWaterLevelManager* WaterManager = nullptr;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
 	float CharacterHeight = 180.0f;
@@ -42,16 +44,40 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
 	float TransitionSpeed = 5.0f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Water Slowdown")
+	bool IsSprinting = false;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Swimming")
+	float SwimmingThreshold = 0.7f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Swimming")
+	bool bIsSwimming = false;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Swimming")
+	float CurrentSubmersionLevel = 0.0f;
+
+	UPROPERTY(BlueprintAssignable, Category = "Swimming")
+	FOnSwimmingStateChanged OnStartSwimming;
+
+	UPROPERTY(BlueprintAssignable, Category = "Swimming")
+	FOnSwimmingStateChanged OnStopSwimming;
+
+protected:
+	virtual void BeginPlay() override;
+
 private:
 	class UCharacterMovementComponent* MovementComponent = nullptr;
 	
-	float DefaultWalkSpeed = 600.0f;
-	float DefaultJumpVelocity = 420.0f;
+	float DefaultJumpVelocity = 400.0f;
 	float DefaultGravityScale = 1.0f;
 	
+	float LastDesiredSpeed = 230.0f;
 	float CurrentSpeedMultiplier = 1.0f;
 	float CurrentJumpMultiplier = 1.0f;
 	float CurrentGravityMultiplier = 1.0f;
 	
 	float CalculateSubmersionLevel();
+	void UpdateSwimmingState(float SubmersionLevel);
+	void UpdateMovementSpeed(float SubmersionLevel, float DeltaTime);
 };
